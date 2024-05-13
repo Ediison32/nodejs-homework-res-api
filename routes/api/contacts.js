36e5e061ca9ea4bf16b2aff5b274ee490bf80e4e
Ediison32 <edisonalvarez32@gmail.com>
2024-05-13 16:33:04 -0500

    fir commit branch hw02-expres

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'firs commmit branch  hw02-expres' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
